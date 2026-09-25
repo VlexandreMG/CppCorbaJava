fabac2ac5d779a45c8fe83d1f6054bcfc34b7d8e
VlexandreMG <dodolashmou@gmail.com>
2026-09-25 23:01:52 +0300

    Yes it's work

--- Cpp/src/ServerMain.cpp
+++ Cpp/src/ServerMain.cpp
@@ -7,7 +7,7 @@ int main(int argc, char* argv[]) {
         CORBA::ORB_var orb = CORBA::ORB_init(argc, argv);
 
         // Intialisation du POA 
-        CORBA::ORB_var obj = orb->resolve_initial_references("RootPOA");
+        CORBA::Object_var obj = orb->resolve_initial_references("RootPOA");
         PortableServer::POA_var poa = PortableServer::POA::_narrow(obj);
 
         // Activer le POA Manager 
