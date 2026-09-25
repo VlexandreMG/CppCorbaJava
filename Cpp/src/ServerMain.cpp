3d234f17712f13cdcf222793058fd0360a431c3f
VlexandreMG <dodolashmou@gmail.com>
2026-09-25 22:55:36 +0300

    Implements server

--- Cpp/src/ServerMain.cpp
+++ Cpp/src/ServerMain.cpp
@@ -7,7 +7,22 @@ int main(int argc, char* argv[]) {
         CORBA::ORB_var orb = CORBA::ORB_init(argc, argv);
 
         // Intialisation du POA 
+        CORBA::ORB_var obj = orb->resolve_initial_references("RootPOA");
+        PortableServer::POA_var poa = PortableServer::POA::_narrow(obj);
 
-    } 
+        // Activer le POA Manager 
+        PortableServer::POAManager_var pman = poa->the_POAManager();
+        pman->activate();
+
+        // Instancier le servant (à l'avenir)
+
+
+        // Lancer le serveur 
+        std::cout << "[SERVEUR IS WORKING]...." << std::endl;
+        orb->run();
+    } catch (const CORBA::Exception& ex) {
+        std::cerr << "[ERREUR CORBA] : " << ex._name() << std::endl;
+        return 1;
+    }
     return 0;
 };
