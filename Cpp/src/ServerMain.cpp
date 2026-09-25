54ce9844c0933fc8e8cad82a278bd8122ca4ab9d
VlexandreMG <dodolashmou@gmail.com>
2026-09-25 17:10:30 +0300

    I will continue at home

--- Cpp/src/ServerMain.cpp
+++ Cpp/src/ServerMain.cpp
@@ -1,3 +1,13 @@
-int main() {
+#include <iostream>
+#include <omniORB4/CORBA.h>
+
+int main(int argc, char* argv[]) {
+    try {
+        // Initialisation de l'ORB 
+        CORBA::ORB_var orb = CORBA::ORB_init(argc, argv);
+
+        // Intialisation du POA 
+
+    } 
     return 0;
 };
